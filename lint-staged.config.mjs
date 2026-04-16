684251724074b7f5bf1f70b428f222457cd04f1f
Krishna Basak <ikrbasak@gmail.com>
2026-04-16 20:25:34 +0530

    chore: update scripts in lint-staged

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,14 +1,8 @@
 // @ts-check
 /** @type {import("lint-staged").Configuration} */
 const config = {
-  '*.ts': (files) => [
-    `bun run fmt -- ${files.join(' ')}`,
-    `bun run lint -- ${files.join(' ')}`,
-    'bunx tsc',
-    'bunx tsc -p tsconfig.build.json',
-    'bun run build',
-  ],
-  '*': (files) => [`bun run fmt -- ${files.filter((f) => !f.endsWith('.ts')).join(' ')}`],
+  '*.ts': (files) => [`bun run lint -- ${files.join(' ')}`],
+  '*': (files) => [`bun run fmt -- ${files.join(' ')}`],
 };
 
 export default config;
